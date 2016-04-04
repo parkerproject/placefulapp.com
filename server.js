a91d7dbeeaf52945db73fa7c07b3f850d1072edd
Parker <parkerproject@gmail.com>
2016-04-03 21:06:32 -0400

    updates

--- server.js
+++ server.js
@@ -10,19 +10,15 @@ var swaggerOptions = {
 var server = new Hapi.Server()
 server.connection({
     host: '0.0.0.0',
-    port: 1300
+    port: 2300
   })
   // Require the routes and pass the server object.
 var routes = require('./server/config/routes')(server)
-
 global.appRoot = path.resolve(__dirname)
-
-// Export the server to be required elsewhere.
+  // Export the server to be required elsewhere.
 module.exports = server
-
-// Bootstrap Hapi Server Plugins, passes the server object to the plugins
-// require('./server/config/plugins')(server)
-
+  // Bootstrap Hapi Server Plugins, passes the server object to the plugins
+  // require('./server/config/plugins')(server)
 server.register([Inert, Vision, Hapi_auth, {
   register: HapiSwagger,
   options: swaggerOptions
@@ -33,7 +29,6 @@ server.register([Inert, Vision, Hapi_auth, {
       html: require('swig')
     }
   })
-
   server.auth.strategy('session', 'cookie', {
     password: 'dancingtomorrow',
     cookie: 'sid-dealsbox',
