16ea1c8ff4fafc6e351416fc081b4c67a5c3a799
Parker <parkerproject@gmail.com>
2016-04-03 20:53:53 -0400

    updates

--- server/controllers/deal.js
+++ server/controllers/deal.js
@@ -3,29 +3,24 @@ require('dotenv').load()
 const req = require('request')
 const collections = ['promotions']
 const db = require('mongojs').connect(process.env.DEALSBOX_MONGODB_URL, collections)
-
 module.exports = {
   index: {
     handler: function (request, reply) {
       req('http://api.placeful.co/promotion?key=' + process.env.API_KEY + '&promotion_id=' + request.params.promotion_id, function (error, response, body) {
         if (!error && response.statusCode == 200) {
           let deal = JSON.parse(body)
-          let facebook_url = 'http://placeful.co/deal/' + request.params.deal_id
-
+          let facebook_url = 'http://placefulapp.com/deal/' + request.params.deal_id
           let tags = deal[0].tags
           let hashtags = tags.map(function (tag) {
             return '#' + tag
           })
-
           hashtags = hashtags.join(' ')
-
           reply.view('deal', {
             deal: deal[0],
             facebook_url: facebook_url,
             facebook_image_url: deal[0].large_image,
             tags: hashtags
           })
-
         }
       })
     }
