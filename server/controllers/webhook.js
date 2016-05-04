2ff72be5689505ba4d4ceb1b53365a0e4d97bdeb
Parker <parkerproject@gmail.com>
2016-05-04 15:55:42 -0400

    updated

--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -24,49 +24,51 @@ module.exports = {
         data.keen = {
           timestamp: new Date().toISOString()
         }
+
+        reply('testing') // remove this afterwards
       }
 
-      db.promotions.find({
-        deal_id: data.deal_id
-      }).limit(1, function (err, result) {
-        if (err) console.log(err)
-        db.webhook.find({
-          deal_id: data.deal_id
-        }).limit(1, function (err, deal) {
-          if (err) console.log(err)
-          if (deal.length === 0) {
-            result[0].score = score
-            result[0].event = 'views'
-
-            delete result[0]['_id']
-
-            db.webhook.save(result[0])
-
-            client.addEvent('views', result, function (err, res) {
-              if (err) {
-                console.log(err)
-              } else {
-                reply('success')
-              }
-            })
-          } else {
-            db.webhook.findAndModify({
-              query: {
-                deal_id: data.deal_id
-              },
-              update: {
-                $inc: {
-                  score: score
-                }
-              },
-              new: true
-            }, function (err, doc, lastErrorObject) {
-              if (err) console.log(err)
-              reply('update successful')
-            })
-          }
-        })
-      })
+    // db.promotions.find({
+    //   deal_id: data.deal_id
+    // }).limit(1, function (err, result) {
+    //   if (err) console.log(err)
+    //   db.webhook.find({
+    //     deal_id: data.deal_id
+    //   }).limit(1, function (err, deal) {
+    //     if (err) console.log(err)
+    //     if (deal.length === 0) {
+    //       result[0].score = score
+    //       result[0].event = 'views'
+    //
+    //       delete result[0]['_id']
+    //
+    //       db.webhook.save(result[0])
+    //
+    //       client.addEvent('views', result, function (err, res) {
+    //         if (err) {
+    //           console.log(err)
+    //         } else {
+    //           reply('success')
+    //         }
+    //       })
+    //     } else {
+    //       db.webhook.findAndModify({
+    //         query: {
+    //           deal_id: data.deal_id
+    //         },
+    //         update: {
+    //           $inc: {
+    //             score: score
+    //           }
+    //         },
+    //         new: true
+    //       }, function (err, doc, lastErrorObject) {
+    //         if (err) console.log(err)
+    //         reply('update successful')
+    //       })
+    //     }
+    //   })
+    // })
     }
   }
 }
